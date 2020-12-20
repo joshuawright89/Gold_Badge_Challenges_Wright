f46c26ad061fe6d05008a0cedf06d3650332ef0f
joshuawright89 <73801906+joshuawright89@users.noreply.github.com>
2020-12-20 10:31:01 -0500

    Added REPO csproj file for Chal_1_Cafe.  Sorry about that!

---
+++ Gold_Badge_Challenges_1_REPO__________/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gold_Badge_Challenge_1
+{
+    public class MenuItem
+    {
+        public string MenuItemNumber { get; set; }
+        public string MenuItemName { get; }
+        public string MenuItemDescription { get; }
+        public string MenuItemIngredients { get; }
+        public string MenuItemPrice { get; }
+
+        public MenuItem(string menuItemName) { }
+
+        public MenuItem(string menuItemName, string menuItemNumber, string menuItemDescription, string menuItemIngredients, string menuItemPrice)
+        {
+            MenuItemName = menuItemName;
+            MenuItemNumber = menuItemNumber;
+            MenuItemDescription = menuItemDescription;
+            MenuItemIngredients = menuItemIngredients;
+            MenuItemPrice = menuItemPrice;
+        }
+    }
+}
---
+++ Gold_Badge_Challenges_1_REPO__________/MenuItemRepo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Gold_Badge_Challenge_1
+{
+    public class MenuItemRepo
+    {
+        public List<MenuItem> _menu = new List<MenuItem>();
+
+        //CREATE menu item
+        public void AddMenuItemToMenu(MenuItem menuItem)
+        {
+            _menu.Add(menuItem);
+        }
+
+        //Display all menu items
+        public List<MenuItem> GetWholeMenu()
+        {
+            return _menu;
+        }
+
+        //Helper (DELETE)
+        public MenuItem GetItemByNumber(string menuItemNumber)
+        {
+            foreach (MenuItem menuItem in _menu)
+            {
+                if (menuItem.MenuItemNumber == menuItemNumber)
+                {
+                    return menuItem;
+                }
+            }
+            return null;
+        }
+
+        //DELETE menu item
+        public bool DeleteMenuItem(string menuItemNumber)
+        {
+            MenuItem menuItem = GetItemByNumber(menuItemNumber);
+            if (_menu.Remove(menuItem))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
